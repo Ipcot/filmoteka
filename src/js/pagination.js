2fa1b7f037673cc0c1817ffee3267cc44fb3333d
Yurii <yuramasha13@gmail.com>
2022-06-06 13:06:51 +0300

    Adds initial logic and syles for pagination

---
+++ src/js/pagination.js
@@ -0,0 +1,118 @@
+import Pagination from 'tui-pagination';
+
+const container = document.getElementById('pagination');
+const options = {
+  totalItems: 515,
+  itemsPerPage: 10,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+
+  template: {
+    page: '<a href="#" class="tui-page-btn pagination__page">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected pagination__page--current">{{page}}</strong>',
+    moveButton: type => {
+      let tpl = '';
+      let customIconClass = '';
+      let customBtnClass = '';
+      let value = '';
+
+      let orderText = '';
+
+      if (type.type === 'first') {
+        value = '1';
+        customBtnClass = 'pagination__move-btn-first';
+      } else if (type.type === 'last') {
+        value = Math.ceil(options.totalItems / options.itemsPerPage);
+        customBtnClass = 'pagination__move-btn-last';
+      } else if (type.type === 'prev') {
+        customIconClass = 'pagination__icon-left';
+        customBtnClass = 'pagination__move-btn pagination__move-btn--prev';
+      } else if (type.type === 'next') {
+        customIconClass = 'pagination__icon-right';
+        customBtnClass = 'pagination__move-btn pagination__move-btn--next';
+      }
+
+      tpl =
+        `<span class="tui-page-btn tui-is-disabled tui-${type.type} ${customBtnClass}">` +
+        `<span class="tui-ico-${type.type} ${customIconClass}">${value}</span>` +
+        '</span>';
+      return tpl;
+    },
+
+    disabledMoveButton: type => {
+      let tpl = '';
+      let customIconClass = '';
+      let customBtnClass = '';
+      let value = '';
+
+      let orderText = '';
+
+      if (type.type === 'first') {
+        value = '1';
+        customBtnClass = 'pagination__move-btn-first';
+      } else if (type.type === 'last') {
+        value = Math.ceil(options.totalItems / options.itemsPerPage);
+        console.log(value);
+        customBtnClass = 'pagination__move-btn-last';
+      } else if (type.type === 'prev') {
+        customIconClass = 'pagination__icon-left';
+        customBtnClass = 'pagination__move-btn pagination__move-btn--prev';
+      } else if (type.type === 'next') {
+        customIconClass = 'pagination__icon-right';
+        customBtnClass = 'pagination__move-btn pagination__move-btn--next';
+      }
+
+      tpl =
+        `<span class="tui-page-btn tui-is-disabled tui-${type.type} ${customBtnClass}">` +
+        `<span class="tui-ico-${type.type} ${customIconClass}">${value}</span>` +
+        '</span>';
+      return tpl;
+    },
+    moreButton: type => {
+      let customBtnClass = 'pagination__more-btn-prev';
+      if (type.type === 'next') {
+        customBtnClass = 'pagination__more-btn-next';
+      }
+
+      let tpl =
+        `<a href="#" class="tui-page-btn tui-${type.type}-is-ellip custom-class-${type.type} ${customBtnClass}">` +
+        `<span class="tui-ico-ellip">...</span>` +
+        `</a>`;
+      return tpl;
+    },
+  },
+};
+const instance = new Pagination(container, options);
+instance.getCurrentPage();
+
+onPageChange();
+
+instance.on('afterMove', event => {
+  const currentPage = event.page;
+  onPageChange(currentPage);
+});
+
+function onPageChange(currentPage = 1) {
+  let firstBtn = document.querySelector('.pagination__move-btn-first');
+  let lastBtn = document.querySelector('.pagination__move-btn-last');
+
+  let totalPages = Math.floor(options.totalItems / options.itemsPerPage);
+  let totalBatches = Math.ceil(totalPages / options.visiblePages);
+  let currentBatch = Math.ceil(currentPage / options.visiblePages);
+
+  if (currentPage <= options.visiblePages) {
+    firstBtn.classList.add('pagination__move-btn--hidden');
+  } else {
+    firstBtn.classList.remove('pagination__move-btn--hidden');
+  }
+
+  if (currentBatch === totalBatches) {
+    lastBtn.classList.add('pagination__move-btn--hidden');
+  } else {
+    lastBtn.classList.remove('pagination__move-btn--hidden');
+  }
+}
