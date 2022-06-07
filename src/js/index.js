2465e2b9e4253af9d8fe79cdc063a9f79670a57c
Yuriy Pokhyl <woully@gmail.com>
2022-06-07 19:34:46 +0300

    delete card and film container

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,7 @@ import './services/render-library-markup';
 import './modal';
 import './pagination';
 
-import './services/back-to-top'
+import './services/back-to-top';
 
 import './show-movies/get-popular';
 import './show-movies/delegation';
-
-
-// console.log('HELLO');
