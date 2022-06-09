67c8dd9b24cc42bdde3cd9136172b66f120c8388
demetrx <mcbright7447@gmail.com>
2022-06-09 23:26:59 +0300

    pagination works!

--- src/js/show-movies/search-movies.js
+++ src/js/show-movies/search-movies.js
@@ -1,19 +1,20 @@
 import MoviesAPI from '../services/movies-api';
 import renderMarkup from '../utils/render-markup';
-import { resetTotalHits, resetPage } from '../pagination';
+import { resetTotalHits } from '../pagination';
 
 const moviesAPI = new MoviesAPI();
 
 const searchMovies = query => {
   moviesAPI.setSearchQuery(query);
-  getSearched();
-  resetPage();
+  getSearched(1);
 };
 
 export function getSearched(page = 1) {
   moviesAPI.fetchMoviesWithQuery(page).then(({ results, total_results }) => {
     renderMarkup(results);
-    resetTotalHits(total_results);
+    if (page === 1) {
+      resetTotalHits(total_results);
+    }
   });
 }
 
