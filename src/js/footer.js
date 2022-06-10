941fd97c1f3e664912c7932f322a06a390215671
Pavlo-levitskiy <90378679+Pavlo-levitskiy@users.noreply.github.com>
2022-06-10 16:53:56 +0300

    Переробив модалку команди через  hbs.

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,16 +1,17 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[footer-button]'),
-    closeMenuBtn: document.querySelector('[data-modal-team-close]'),
-    Menu: document.querySelector('[data-modal-team ]'),
-    bodyRef: document.querySelector('body'),
-  };
+import teamTemplates from '../templates/our-team.hbs';
+import members from './services/our-team.json';
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+const refs = {
+  openMenuBtn: document.querySelector('.footer__link'),
+  closeMenuBtn: document.querySelector('.modal-content__close-btn-team '),
+};
 
-  function toggleMenu() {
-    refs.Menu.classList.toggle('is-hidden');
-    refs.bodyRef.classList.toggle('no-scroll');
-  }
-})();
+refs.openMenuBtn.addEventListener('click', renderOurTEam);
+refs.closeMenuBtn.addEventListener('click');
+
+function renderOurTEam() {
+  const markup = teamTemplates(members);
+  refs.openMenuBtn.insertAdjacentHTML('afterbegin', markup);
+  return markup;
+}
+export default renderOurTEam;
