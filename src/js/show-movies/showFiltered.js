e562b18236ccc7f2612fd5131c126aa6aa31c50e
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-06-12 11:36:43 +0300

    genre filter markup

--- src/js/show-movies/showFiltered.js
+++ src/js/show-movies/showFiltered.js
@@ -26,3 +26,29 @@ function showFitered(page = 1) {
 export default searchWithFilter;
 
 // setTimeout(() => searchWithFilter([28, 12, 16]), 3000);
+
+const filterBtn = document.querySelector('.genre-filter__btn');
+ const filterModal = document.querySelector('.filter-modal');
+ const filterClose = document.querySelector('.filter-close');
+ const okBtn = document.querySelector('.filter-btn');
+
+filterBtn.addEventListener('click', openFilterModal);
+filterClose.addEventListener('click', closeFilterModal);
+
+okBtn.addEventListener('click', () => {
+  const selectedGenres = document.querySelectorAll('.custom-checkbox') ;
+  const genreIds = Array.from(selectedGenres).map(genre => genre.dataset.id);
+  showSpinner(true);
+  setTimeout(() =>  searchWithFilter(genreIds), 1000);
+  closeFilterModal();
+}
+);
+
+function openFilterModal() {
+  filterModal.classList.remove('is-hidden');
+
+}
+
+function closeFilterModal() {
+  filterModal.classList.add('is-hidden');
+}
