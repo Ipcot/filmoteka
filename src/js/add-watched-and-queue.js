020cc21c09480ce604b83d82a775ea517e4df541
Yuriy Pokhyl <woully@gmail.com>
2022-06-12 15:22:16 +0300

    fix key

--- src/js/add-watched-and-queue.js
+++ src/js/add-watched-and-queue.js
@@ -1,13 +1,13 @@
 import { addMovieIdToLocalStorage, addMovieToQueue } from './add-to-localstorage';
 
 export function onBtnWatchedClick(e) {
-  const WATCHED_ID_KEY = 'watched id';
+  const WATCHED_ID_KEY = 'watched';
   const movieId = e.target.dataset.id;
   addMovieIdToLocalStorage(WATCHED_ID_KEY, movieId);
 }
 
 export function onBtnQueueClick(e) {
-  const QUEUE_ID_KEY = 'queue id';
+  const QUEUE_ID_KEY = 'queue';
   const movieId = e.target.dataset.id;
   addMovieToQueue(QUEUE_ID_KEY, movieId);
 }
